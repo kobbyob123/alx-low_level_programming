0852e13c9c4cb2c3f822615a378d2d8586380fff
Kwabena Obeng Ackor <popolapii@gmail.com>
2022-09-09 14:29:45 -0700

    Task 1-6

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+/**
+ * main - prints the alphabets in all lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
